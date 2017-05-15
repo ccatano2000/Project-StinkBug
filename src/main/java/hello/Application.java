25670c3e1abd6e9e7f8b90fe291708bba8aa41cd
ccatano2000 <christopher.catano@emc.com>
2017-05-15 13:25:57 -0700

    ITS ALIIIIIIIIVE

---
+++ src/main/java/hello/Application.java
@@ -0,0 +1,15 @@
+package hello;
+
+/**
+ * Created by elm on 3/22/17.
+ */
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
